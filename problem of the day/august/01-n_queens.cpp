486ce2f41975d9e9820d13243b58523b78d894a0
Vivek <vgamit637@gmail.com>
2025-08-01 23:43:46 +0530

    Create 01-n_queens.cpp

---
+++ problem of the day/august/01-n_queens.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+
+    bool isSafe(vector<string>&board, int row, int col, int n){
+        //horizontal
+        for(int j=0; j<n; j++){
+            if(board[row][j] == 'Q'){
+                return false;
+            }
+        }
+        //vertical 
+        for(int i=0; i<n; i++){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        //left diagonal 
+        for(int i=row,j=col; i>=0 && j>=0; i--,j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        //right diagonal
+        for(int i=row,j=col; i>=0 && j<n; i--,j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void nqueen(vector<string>&board, int row, int n, vector<vector<string>>&ans){
+        if(row == n){
+            ans.push_back({board});
+            return;
+        }
+
+        for(int j=0; j<n; j++){
+            if(isSafe(board,row,j,n)){
+                board[row][j] = 'Q';
+                nqueen(board,row+1,n,ans);
+                board[row][j] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string>board(n,string(n,'.'));
+        vector<vector<string>>ans;
+
+        nqueen(board,0,n,ans);
+        return ans;
+    }
+};

7b55bb1db778cf6c4716aca20c20f5138b4e5912
vivek-gamit <vgamit637@gmail.com>
2026-04-10 09:26:39 +0530

    optimize the code

--- Increase decrease logic/02_pattern.js
+++ Increase decrease logic/02_pattern.js
@@ -8,7 +8,7 @@
 #
 */
 
-let n=4
+let n=5
 for(let i=1; i<=n; i++){
     let row=""
     for(let j=1; j<=i; j++){
@@ -17,6 +17,8 @@ for(let i=1; i<=n; i++){
     console.log(row);
     
 }
+
+/* it is also posible using only logic in first loog where we only write condition for i=n-1 === 3 ans decrease one by one i-- */
 for(let i=1; i<=n-1; i++){
     let row=""
     for(let j=1; j<=n-i; j++){

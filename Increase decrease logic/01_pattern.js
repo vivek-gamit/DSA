6e74658c29ded740cf02258c7fc2e1840134aab0
vivek-gamit <vgamit637@gmail.com>
2026-04-10 08:58:57 +0530

    decrease logic

---
+++ Increase decrease logic/01_pattern.js
@@ -0,0 +1,17 @@
+/* half square
+#####
+####
+###
+##
+#
+*/
+
+let n=5
+ for(let i=1; i<=n; i++){
+    let row="";
+    for(let j=1; j<=n+1-i; j++){
+        row+="#"
+    }
+    console.log(row);
+    
+ }
